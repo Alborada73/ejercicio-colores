a0337f5f5cd4fbe6398de3babae192009ca3027a
yasna angulo <yasna.angulo>
2023-04-27 22:33:48 -0400

    2da parte

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 let colores = []
 for (let index = 1; index <= 5; index++) {
-    var color = prompt(ìngresar color nro (${index}))
-    color = color.toLocaleLowerCase()
-    if(!colores.includes(color))
+    var color = prompt(`ìngresar color Nro (${index})`)
+    color = color.toLowerCase()
+    if(!colores.includes(color)) {
     colores.push(color)
-}
+    
+    }
 }
 
 console.log(colores);
