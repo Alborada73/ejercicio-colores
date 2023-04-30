21cb04adc267306d98ea001ea0f924e9007ecb15
yasna angulo <yasna.angulo>
2023-04-29 22:38:31 -0400

    tarea colores finalizada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,43 @@
-let colores = []
+var colores = []
+var listado = document.querySelector("#listado-colores tbody")
 for (let index = 1; index <= 5; index++) {
-    var color = prompt(`ìngresar color Nro (${index})`)
-    color = color.toLowerCase()
-    if(!colores.includes(color)) {
-    colores.push(color)
-    
-    }
+    do {
+        var color = prompt(`ìngresar color Nro (${index})`)
+        color = color.toLowerCase()
+
+        var agregar = !colores.includes(color)
+        if(agregar) {
+            colores.push(color)        
+        } else {
+            alert ("Hubo un error registrando el color. Intente nuevamente")
+        }
+    } while (!agregar); 
 }
 
-console.log(colores);
+mostrarColores();
+
+function mostrarColores() {
+    listado.innerHTML = ""
+    colores.forEach(color, index) => {
+        listado.innerHTML += `
+            <tr>
+                <td>${color}</td>
+                <td style="background: ${color}"></td>
+                <td><button type="button" class="btn btn-sm btn-danger" onClick="eliminar(${index})">Eliminar</button></td>
+            </tr> 
+            `
+      
+    })
+}
+
+function eliminar(indice) {
+    if(confirm(`¿Seguro que desea eliminar el color ${colores[indice]}?`)){
+        var eliminado = colores.splice(indice, 1)
+        alert(`Usted ha eliminado el color ${eliminado[0]}`)
+        mostrarColores()
+
+    }
+
+
+}
+
